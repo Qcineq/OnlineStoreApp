1af7e3f3de1e885a82f60377eea36635f8a0c6ba
Kuba <jakub.kucinski@icloud.com>
2023-11-19 23:47:08 +0100

    Createrd FooterView, added app icon, launchScreen

---
+++ OnlineStoreApp/Utility/Constant.swift
@@ -0,0 +1,24 @@
+//
+//  Constant.swift
+//  OnlineStoreApp
+//
+//  Created by Jakub Kuciński on 19/11/2023.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+
+
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(red: 110/255, green: 110/255, blue: 110/255)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
---
+++ OnlineStoreApp/View/Home/FooterView.swift
@@ -0,0 +1,36 @@
+//
+//  FooterView.swift
+//  OnlineStoreApp
+//
+//  Created by Jakub Kuciński on 19/11/2023.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Text("Salon Broni - Twój sklep z bronią w Warszawie")
+                .foregroundStyle(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            
+            Image("logo-main-scaled")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+                .layoutPriority(0)
+            
+            Text("Copyright © Salon Broni\nWszelkie prawa zastrzeżone")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundStyle(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    FooterView()
+}
