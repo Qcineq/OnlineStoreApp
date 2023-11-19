91a34fbf16e1e4d23f4596a757edd3a3f243b907
Kuba <jakub.kucinski@icloud.com>
2023-11-19 22:56:11 +0100

    Initial Commit

---
+++ OnlineStoreApp/OnlineStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineStoreAppApp.swift
+//  OnlineStoreApp
+//
+//  Created by Jakub Kuciński on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct OnlineStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
